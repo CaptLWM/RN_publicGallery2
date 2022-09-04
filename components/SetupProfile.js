10aa5cb2f5bac30a0ba2c9e6deb698fd6a6b5968
CaptLWM <wonmin275@gmail.com>
2022-09-04 14:00:47 +0900

    RN 8.5.1

--- components/SetupProfile.js
+++ components/SetupProfile.js
@@ -1,6 +1,7 @@
 import {useNavigation, useRoute} from '@react-navigation/native';
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import {Image, Platform, Pressable, StyleSheet, View} from 'react-native';
+import {launchImageLibrary} from 'react-native-image-picker';
 import {useUserContext} from '../contexts/UserContext';
 import {signOut} from '../lib/auth';
 import {createUser} from '../lib/users';
@@ -12,6 +13,7 @@ const SetupProfile = () => {
   console.log(displayName, '????');
   const navigation = useNavigation();
   const {setUser} = useUserContext();
+  const [response, setResponse] = React.useState(null);
 
   const {params} = useRoute();
   const {uid} = params || {};
@@ -31,9 +33,36 @@ const SetupProfile = () => {
     navigation.goBack();
   };
 
+  const onSelectImage = () => {
+    launchImageLibrary(
+      {
+        mediaType: 'photo',
+        maxWidth: 512,
+        maxHeight: 512,
+        includeBase64: Platform.OS === 'android',
+      },
+      res => {
+        if (res.didCancel) {
+          //취소했을 경우
+          return;
+        }
+        setResponse(res);
+      },
+    );
+  };
+
   return (
     <View style={styles.block}>
-      <View style={styles.circle} />
+      <Pressable onPress={onSelectImage}>
+        <Image
+          style={styles.circle}
+          source={
+            response
+              ? {uri: response?.assets[0]?.uri}
+              : require('../assets/user.png') // 기본이미지 설정
+          }
+        />
+      </Pressable>
       <View style={styles.form}>
         <BorderedInput
           placeholder="닉네임"
