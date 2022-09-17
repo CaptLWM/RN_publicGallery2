9fb0e019ced56c7d186ffdcf4ed11fbbfeab72f7
CaptLWM <wonmin275@gmail.com>
2022-09-18 00:07:26 +0900

    RN 9.3 post

--- index.js
+++ index.js
@@ -5,5 +5,6 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import 'react-native-get-random-values'; // 파일 업로드 고유 아이디 생성
 
 AppRegistry.registerComponent(appName, () => App);
