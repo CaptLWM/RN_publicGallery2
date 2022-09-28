ee5b7ad5b158fc56a263a44c560154a3b62561ac
CaptLWM <wonmin275@gmail.com>
2022-09-28 23:38:48 +0900

    RN 9.8 logOut

--- screens/MyProfileScreen.js
+++ screens/MyProfileScreen.js
@@ -3,6 +3,7 @@ import React, {useEffect} from 'react';
 import {View} from 'react-native';
 import Profile from '../components/Profile';
 import {useUserContext} from '../contexts/UserContext';
+import IconRightButton from '../components/IconRightButton';
 
 const MyProfileScreen = () => {
   const {user} = useUserContext();
@@ -11,6 +12,12 @@ const MyProfileScreen = () => {
   useEffect(() => {
     navigation.setOptions({
       title: user.displayName,
+      headerRight: () => (
+        <IconRightButton
+          name="settings"
+          onPress={() => navigation.push('Setting')}
+        />
+      ),
     });
   }, [navigation, user]);
   return <Profile userId={user.id} />;
